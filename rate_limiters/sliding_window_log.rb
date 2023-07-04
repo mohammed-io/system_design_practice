98fead1a88b409ac40a054ff5684117449c242b8
Mohammed A <mohd@hey.com>
2023-07-04 10:01:03 +0300

    A correct implementation to the sliding window log rate limiter

--- rate_limiters/sliding_window_log.rb
+++ rate_limiters/sliding_window_log.rb
@@ -1,22 +1,35 @@
 require 'minitest/autorun'
 require 'async'
 
+class FakeTime
+  def self.timestamp
+    @timestamp ||= 0
+  end
+
+  def self.advance(seconds = 1)
+    @timestamp = timestamp + seconds
+  end
+end
+
 class SlidingWindowLog
   attr_reader :log
 
-  MAX_REQUESTS_PER_WINDOW = 2
+  MAX_REQUESTS_PER_WINDOW = 3
+  WINDOW_SIZE_IN_SECONDS = 10
 
   def initialize
     @log = []
   end
 
   def handle(_)
-    if @log.empty?
-      @log << current_window
-      true
-    elsif @log.select { _1 == current_window }.size < MAX_REQUESTS_PER_WINDOW
-      @log = log.last(2)
-      @log << current_window
+    @log << FakeTime.timestamp
+
+    @log.reject! { |timestamp| p(current_window - timestamp >= WINDOW_SIZE_IN_SECONDS) }
+    puts @log.inspect
+    puts current_window
+    puts '-----'
+
+    if @log.size <= MAX_REQUESTS_PER_WINDOW
       true
     else
       false
@@ -24,7 +37,7 @@ class SlidingWindowLog
   end
 
   def current_window
-    Time.now.to_i
+    (FakeTime.timestamp / WINDOW_SIZE_IN_SECONDS) * WINDOW_SIZE_IN_SECONDS + 1
   end
 end
 
@@ -33,19 +46,33 @@ class SlidingWindowLogTest < Minitest::Test
     log = SlidingWindowLog.new
     assert log.handle(:request)
     assert log.handle(:request)
+    assert log.handle(:request)
     refute log.handle(:request)
   end
 
   def test_it_handles_request_after_the_time_window_passed
-    Async do
-      log = SlidingWindowLog.new
-      assert log.handle(:request)
-      assert log.handle(:request)
-      refute log.handle(:request)
-      sleep 1
-      assert log.handle(:request)
-      assert log.handle(:request)
-      refute log.handle(:request)
-    end
+    log = SlidingWindowLog.new
+    assert log.handle(:request)
+    FakeTime.advance(5)
+    assert log.handle(:request)
+    FakeTime.advance(2)
+    assert log.handle(:request)
+    refute log.handle(:request)
+
+    FakeTime.advance(12)
+    refute log.handle(:request)
+
+    FakeTime.advance(1)
+    assert log.handle(:request)
+    FakeTime.advance(9) # last second in the 2nd window
+    assert log.handle(:request)
+    refute log.handle(:request) # Burst prevented.
+
+    # After long time, i.e. the previous window has passed;
+    FakeTime.advance(100)
+    assert log.handle(:request)
+    assert log.handle(:request)
+    assert log.handle(:request)
+    refute log.handle(:request)
   end
 end
